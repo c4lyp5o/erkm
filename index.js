f3024cd7eb5e6b257458a57a3a36b3a91396bb51
c4lyp5o <calypso.hellhammer@gmail.com>
2022-07-25 16:25:41 +0800

    created dummy erkm data

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const port = process.env.PORT || 3000;
+const server = express();
+
+server.use(cors());
+
+server.use((_, res, next) => {
+  res.setHeader(
+    'Cache-Control',
+    'public, max-age=0, s-maxage=86400, stale-while-revalidate'
+  );
+  next();
+});
+
+server.use(express.json());
+
+server.use(routes);
+
+server.listen(port, () => {
+  console.log('Server running at port:', port);
+});
---
+++ routes.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const { data } = require('./ERKM-dummy');
+const { Router } = require('express');
+const router = Router();
+
+router.get('/', (req, res) => {
+  res.send('Test erkm server');
+});
+
+router.get('/data', (req, res) => {
+  console.log(data);
+  res.send(data);
+});
+
+router.get('/sr', (req, res) => {
+  const sekolahrendah = data.sekolahRendah;
+  res.send(sekolahrendah);
+});
+
+router.get('/sr/:id', (req, res) => {
+  const sekolahrendah = data.sekolahRendah;
+  const singleSekolah = sekolahrendah.find(
+    (sekolah) => sekolah.id === parseInt(req.params.id)
+  );
+  res.json({ singleSekolah });
+});
+
+router.get('/sm', (req, res) => {
+  const sekolahmenengah = data.sekolahMenengah;
+  res.send(sekolahmenengah);
+});
+
+router.get('/sm/:id', (req, res) => {
+  const sekolahmenengah = data.sekolahMenengah;
+  const singleSekolah = sekolahmenengah.find(
+    (sekolah) => sekolah.id === parseInt(req.params.id)
+  );
+  res.json({ singleSekolah });
+});
+
+module.exports = router;
